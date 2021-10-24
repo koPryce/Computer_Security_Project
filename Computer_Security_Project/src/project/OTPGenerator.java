79470d6bc54999efa3894483c298f96a95a663af
KPryce <prycekevaughn@gmail.com>
2021-10-23 23:42:21 -0500

    Functional OTP Code

--- Computer_Security_Project/src/project/OTPGenerator.java
+++ Computer_Security_Project/src/project/OTPGenerator.java
@@ -1,12 +1,26 @@
 package project;
 
 import java.util.Random;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 public class OTPGenerator {
+
+	 public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+	 public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
 	public static void main(String[] args) {
 		String oneTimePassword = generateOTP(5); //Call to the generateOTP method.
 		System.out.println("Generated One Time Password: "+ oneTimePassword); //Displays the OTP.
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+	     Message message = Message.creator(
+	    		 new com.twilio.type.PhoneNumber("+1876"),
+	             new com.twilio.type.PhoneNumber("+17408471870"),
+	             "This is your One Time Password:" + oneTimePassword)
+	         .create();
+
+	     System.out.println(message.getSid());
 	}
 
 	public static String generateOTP(int length) {
