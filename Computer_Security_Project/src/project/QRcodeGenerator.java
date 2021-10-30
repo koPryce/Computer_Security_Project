75507d847731355004968f7dd0ed1194f684e497
KPryce <prycekevaughn@gmail.com>
2021-10-30 05:43:45 -0500

    Putting together OTP and Email

---
+++ Computer_Security_Project/src/project/QRcodeGenerator.java
@@ -0,0 +1,104 @@
+package project;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+import project.WebcamQR;
+import project.SendEmail;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRcodeGenerator {
+	static String PATH;	
+	public void generateQRcode(String credentials,String recipient)  
+	{  
+		MakeDirectory();
+		try {
+			int WIDTH = 200;
+			int HEIGHT = 200;
+			Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();  
+			//generates QR code with Low level(L) error correction capability  
+			hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L); 
+			//path where we want to get QR Code  
+			String path = PATH+"\\Quote.png";  
+			//Encoding charset to be used  
+			String charset = "UTF-8";  
+			
+			
+		//the BitMatrix class represents the 2D matrix of bits  
+		//MultiFormatWriter is a factory class that finds the appropriate Writer subclass for the BarcodeFormat requested and encodes the barcode with the supplied contents.  
+		BitMatrix matrix = new MultiFormatWriter().encode(new String(credentials.getBytes(charset), charset), BarcodeFormat.QR_CODE, WIDTH,HEIGHT);
+		
+		MatrixToImageWriter.writeToPath(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path).toPath()); 
+		SendEmail.sendMail(recipient);
+		File currentFile = new File(path);
+	    currentFile.delete();
+		}
+		catch(WriterException | IOException exception) {
+			exception.printStackTrace();
+			
+		}
+		 
+	}  
+	
+	public String[] readQRcode()  
+	{  
+		String [] arr = {""};
+		try {
+			JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			int r = j.showOpenDialog(null);
+			
+			if (r == JFileChooser.APPROVE_OPTION)
+
+			{
+				BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(j.getSelectedFile().getAbsolutePath())))));  
+				Result rslt = new MultiFormatReader().decode(binaryBitmap);
+				arr = rslt.getText().split("%");
+			}
+			return arr;  
+		}
+		catch(IOException | NotFoundException ex) {
+			ex.printStackTrace();
+		} 
+		return arr;
+
+	} 
+	public String[] readByCamera() {
+
+		WebcamQR qr =  new WebcamQR();
+		return qr.startScan();
+			
+	}
+	
+	private static void MakeDirectory() {
+		PATH = System.getProperty("user.home")+"\\QRcode";
+		System.out.println(System.getProperty("user.home"));
+		File file = new File(PATH);
+	      //Creating the directory
+	      boolean bool = file.mkdir();
+	      if(bool){
+	         System.out.println("Directory created successfully");
+	      }else{
+	         System.out.println("Sorry couldn’t create specified directory");
+	      }
+	}
+}
---
+++ Computer_Security_Project/src/project/SendEmail.java
@@ -0,0 +1,79 @@
+package project;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+
+
+
+public class SendEmail {
+	public static void sendMail(String recipient) {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable","true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		
+		String account = "compsecgroup3@gmail.com";
+		String password = "com$3cur1ty";
+		
+		Session session = Session.getInstance(properties,new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(account,password);
+			}
+		});
+		
+		Message message = prepareMessage(session,account,recipient);
+		try {
+			Transport.send(message);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static Message prepareMessage(Session session,String account,String recipient) {
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(account));
+			message.setRecipient(Message.RecipientType.TO,new InternetAddress(recipient));
+			message.setSubject("QR Code Authentication");
+			message.setText("Please see QR code in the attachment below.");
+			Multipart content = new MimeMultipart();
+			MimeBodyPart part = new MimeBodyPart();
+			try {
+				String PATH = System.getProperty("user.home")+"\\QRcode";
+				part.attachFile(PATH+"\\Quote.png");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			content.addBodyPart(part);
+			message.setContent(content);
+			return message;
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	}
+		return null;
+
+}
+
+}
